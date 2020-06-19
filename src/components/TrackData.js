546794fb73eb56e2b8aa44103e854bbb8c1680ec
Dmitry Kutin <dmitrykutin@outlook.com>
2020-06-18 22:47:45 -0400

    Working version

--- src/components/TrackData.js
+++ src/components/TrackData.js
@@ -1,30 +1,76 @@
 import React, { Component } from 'react';
 import CanvasJSReact from '../assets/canvasjs.react';
 
-var CanvasJS = CanvasJSReact.CanvasJS;
+// var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class TrackData extends Component {
 
     render() {
-      {console.log(this.props.tracks)}
-      {console.log(this.props.features)}
       const options = {
-        theme: "light2",
+        theme: "light2", 
         title: {
           text: "Your Music Change Over 50 Songs"
         },
-        data: [{				
-                  type: "column",
-                  dataPoints: [
-                      { label: "Apple",  y: 10  },
-                      { label: "Orange", y: 15  },
-                      { label: "Banana", y: 25  },
-                      { label: "Mango",  y: 30  },
-                      { label: "Grape",  y: 28  }
-                  ]
-         }]
+        axisY: {
+          title: "Multiple",
+          includeZero: false,
+        },
+        axisX: {
+          title: "Song",
+          interval: 1
+        },
+        data: [
+          {				
+          type: "line",
+          name: "",
+          dataPoints: []
+        },
+        {				
+          type: "line",
+          name: "",
+          dataPoints: []
+        },
+        {				
+          type: "line",
+          name: "",
+          dataPoints: []
+        },
+        {				
+          type: "line",
+          name: "",
+          dataPoints: []
+        },
+        {				
+          type: "line",
+          name: "",
+          dataPoints: []
+        },
+        ]
      }
+
+      var label;
+      var point;
+      var property;
+      const type = [
+        "danceability",
+        "energy",
+        "liveness",
+        "speechiness",
+        "valence"
+      ]
+      
+      for (var i = 0; i < this.props.tracks.length; i++) {
+        label = this.props.tracks[i].track.name;
+        for (var j = 0; j < type.length; j++) {
+          property = type[j];
+          point = this.props.features[i][property];
+          options.data[j].name = property;
+          options.data[j].dataPoints.push({label : label, y : point});
+        }
+      }
+
+      console.log(options);
       
      return (
         <div>
