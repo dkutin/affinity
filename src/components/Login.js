d9d8e423faed2d96ad0ea6c649c4c97cf0399516
Dmitry Kutin <dmitrykutin@outlook.com>
2020-08-26 22:12:53 -0400

    Update login url

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,15 +4,17 @@ import React, { Component } from 'react';
 class Login extends Component {
 
     render() {
-      return (<div className="login">  
-      <h2> Ever wonder how your music taste has progressed over the years? </h2>
-      <p> This web application, built in React, allows for a visual representation of your change in music taste over the years. </p>
-      <a href={"https://auth-server-dot-affinity-281921.uc.r.appspot.com/login"}  >
-        <div className="home-button"> 
-          Lets get started
+      return (
+        <div className="login">  
+          <h2> Ever wonder how your music taste has progressed over the years? </h2>
+          <p> This web application, built in React, allows for a visual representation of your change in music taste over the years. </p>
+          <a href={"https://affinity-281921.uc.r.appspot.com/login"}  >
+            <div className="home-button"> 
+              Lets get started
+            </div>
+          </a>
         </div>
-      </a>
-      </div>);
+      );
     }
 }
 
